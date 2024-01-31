45ba85c13ef39db22c5d11181f7cef2495e3d1ed
Prakash-102 <142473108+Prakash-102@users.noreply.github.com>
2024-01-31 20:11:29 +0530

    Add files via upload

---
+++ Classroom.java
@@ -0,0 +1,23 @@
+
+//change a value in program ?:- 
+import java.util.*;
+
+public class Classroom {
+
+    public static void main(String[] args) {
+        // Swap-values exchanges
+        int a = 5;
+        int b = 10;
+        // swap
+        int temp = a;
+        a = b;
+        b = temp;
+        System.out.println("a = " + a);
+        System.out.println("b = " + b);
+    }
+}
+/*
+ * Output:-
+ * a = 10
+ * b = 5
+ */
